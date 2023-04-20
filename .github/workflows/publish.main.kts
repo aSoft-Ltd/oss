8024470f02024fdffb71a13e3c1f66bdada252b5
andylamax <andylamax@programmer.net>
2023-04-20 14:54:48 +0300

    Removed targeting-all=true to see if its other targets that bringin complications

--- .github/workflows/publish.main.kts
+++ .github/workflows/publish.main.kts
@@ -16,27 +16,27 @@ import io.github.typesafegithub.workflows.dsl.workflow
 import io.github.typesafegithub.workflows.yaml.toYaml
 
 data class RootProject(
-        val name: String,
-        val path: String,
-        val subs: List<String>,
-        val repo: String = path
+    val name: String,
+    val path: String,
+    val subs: List<String>,
+    val repo: String = path
 )
 
 val projects = listOf(
-        RootProject("functions", "functions", listOf("core")),
-        RootProject("kommander", "kommander", listOf("core", "coroutines")),
-        RootProject("lexi", "lexi", listOf("api", "console", "file")),
-        RootProject("lexi-test", "lexi", listOf("android")),
-        RootProject("kollections", "kollections", listOf("interoperable", "atomic")),
-        RootProject("kevlar", "kevlar", listOf("core")),
-        RootProject("kase", "kase", listOf("core")),
-        RootProject("koncurrent-executors", "koncurrent", listOf("core", "coroutines", "mock")),
-        RootProject("koncurrent-later", "koncurrent", listOf("core", "coroutines", "test")),
-        RootProject("keep", "keep", listOf("api", "browser", "file", "mock", "react-native")),
+    RootProject("functions", "functions", listOf("core")),
+//    RootProject("kommander", "kommander", listOf("core", "coroutines")),
+//    RootProject("lexi", "lexi", listOf("api", "console", "file")),
+//    RootProject("lexi-test", "lexi", listOf("android")),
+//    RootProject("kollections", "kollections", listOf("interoperable", "atomic")),
+//    RootProject("kevlar", "kevlar", listOf("core")),
+//    RootProject("kase", "kase", listOf("core")),
+//    RootProject("koncurrent-executors", "koncurrent", listOf("core", "coroutines", "mock")),
+//    RootProject("koncurrent-later", "koncurrent", listOf("core", "coroutines", "test")),
+//    RootProject("keep", "keep", listOf("api", "browser", "file", "mock", "react-native")),
+    // =======================
 //    RootProject("live", "live", listOf("core", "compose", "coroutines", "react", "test")),
 
 //    RootProject("viewmodel", "viewmodel", listOf("core")),
-//    RootProject("cache", "cache", listOf("api", "browser", "file", "mock", "react-native")),
 //
 //    RootProject("formatter", "formatter", listOf("core")),
 //    RootProject("kash", "kash", listOf("currency", "money")),
@@ -51,16 +51,6 @@ val projects = listOf(
 //
 //    RootProject("mailer", "mailer", listOf("api", "mock", "smtp")),
 //
-//    RootProject("bitframe-actor", "bitframe", listOf("core", "app", "user", "space")),
-//    RootProject("bitframe-dao", "bitframe", listOf("core", "mock", "mongo", "file")),
-//    RootProject("bitframe", "bitframe", listOf("dao")),
-//    RootProject("bitframe-service-builder", "bitframe", listOf("core", "daod", "rest")),
-//    RootProject("bitframe-service-builder-api", "bitframe", listOf("core", "ktor", "mock")),
-//    RootProject("bitframe-service-builder-sdk-client", "bitframe", listOf("core", "react" /* "mock",*/)),
-//    RootProject("bitframe-service-builder-sdk-server", "bitframe", listOf("core")),
-//    RootProject("bitframe-api", "bitframe", listOf("core" /* "ktor", "mock" */)),
-//    RootProject("bitframe-sdk-server", "bitframe", listOf("core", "ktor", "test")),
-//
 //    // math libs
 //    RootProject("math", "math", listOf("core")),
 //    RootProject("math-spatial", "math", listOf("core")),
@@ -74,31 +64,32 @@ fun JobBuilder<JobOutputs.EMPTY>.setupAndCheckout(rp: RootProject) {
     uses(CheckoutV3(submodules = true))
     uses(SetupJavaV3(javaVersion = "18", distribution = SetupJavaV3.Distribution.Corretto))
     run(
-            name = "Make ./gradlew executable",
-            command = "chmod +x ./gradlew",
-            workingDirectory = rp.path,
+        name = "Make ./gradlew executable",
+        command = "chmod +x ./gradlew",
+        workingDirectory = rp.path,
     )
 }
 
 fun WorkflowBuilder.buildProject(rp: RootProject) = job(
-        id = "${rp.name}-builder", runsOn = RunnerType.MacOSLatest
+    id = "${rp.name}-builder", runsOn = RunnerType.MacOSLatest
 ) {
     setupAndCheckout(rp)
     rp.subs.forEach {
         val task = ":${rp.name}-$it:build"
         uses(
-                name = "./gradlew $task",
-                action = GradleBuildActionV2(arguments = task, buildRootDirectory = "./${rp.path}")
+            name = "./gradlew $task",
+            action = GradleBuildActionV2(arguments = task, buildRootDirectory = "./${rp.path}")
         )
     }
 }
 
 fun WorkflowBuilder.publishProject(rp: RootProject, after: Job<JobOutputs.EMPTY>) = job(
-        id = "${rp.name}-publisher", runsOn = RunnerType.MacOSLatest, needs = listOf(after)
+    id = "${rp.name}-publisher", runsOn = RunnerType.MacOSLatest, needs = listOf(after)
 ) {
     setupAndCheckout(rp)
 
-    val argument = rp.subs.joinToString(separator = " ") { ":${rp.name}-$it:publishToSonatype" } //+ " closeAndReleaseStagingRepository"
+    val argument =
+        rp.subs.joinToString(separator = " ") { ":${rp.name}-$it:publishToSonatype" } //+ " closeAndReleaseStagingRepository"
     uses(
         name = "publishing " + rp.subs.joinToString(", ") { "${rp.name}-$it" },
         action = GradleBuildActionV2(arguments = argument, buildRootDirectory = "./${rp.path}")
@@ -106,14 +97,14 @@ fun WorkflowBuilder.publishProject(rp: RootProject, after: Job<JobOutputs.EMPTY>
 }
 
 val workflow = workflow(
-        name = "Build, Cache then Publish", on = listOf(Push(branches = listOf("main"))), sourceFile = __FILE__.toPath(),
-        env = linkedMapOf(
-                "ASOFT_MAVEN_PGP_PRIVATE_KEY" to expr { secrets["ASOFT_MAVEN_PGP_PRIVATE_KEY"].toString() },
-                "ASOFT_MAVEN_PGP_PASSWORD" to expr { secrets["ASOFT_MAVEN_PGP_PASSWORD"].toString() },
-                "ASOFT_NEXUS_PASSWORD" to expr { secrets["ASOFT_NEXUS_PASSWORD"].toString() },
-                "ASOFT_NEXUS_USERNAME" to expr { secrets["ASOFT_NEXUS_USERNAME"].toString() },
-                "TARGETING_ALL" to "true"
-        ),
+    name = "Build, Cache then Publish", on = listOf(Push(branches = listOf("main"))), sourceFile = __FILE__.toPath(),
+    env = linkedMapOf(
+        "ASOFT_MAVEN_PGP_PRIVATE_KEY" to expr { secrets["ASOFT_MAVEN_PGP_PRIVATE_KEY"].toString() },
+        "ASOFT_MAVEN_PGP_PASSWORD" to expr { secrets["ASOFT_MAVEN_PGP_PASSWORD"].toString() },
+        "ASOFT_NEXUS_PASSWORD" to expr { secrets["ASOFT_NEXUS_PASSWORD"].toString() },
+        "ASOFT_NEXUS_USERNAME" to expr { secrets["ASOFT_NEXUS_USERNAME"].toString() },
+//        "TARGETING_ALL" to "false"
+    ),
 ) {
 //    val buildJobs = projects.map { buildProject(it) }
 //    val rendezvous = job(id = "rendezvous", runsOn = RunnerType.UbuntuLatest, needs = buildJobs) {
